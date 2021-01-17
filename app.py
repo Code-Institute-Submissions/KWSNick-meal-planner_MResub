f1015953f19b04542cb8d0c80d3808fe589be7ae
Nicholas Bowley <nicholasbowley@hotmail.com>
2021-01-17 17:01:12 +0000

    Created instance of Flask in app.py, configured instance parameters based on env.py variables, created instance of PyMongo, provided initial route for Flask and route for /recipes. Tested Mongo connection in /recipes by finding all recipes in collection and returning them to screen vai Jinja templating into simple boilerplate HTML.

--- app.py
+++ app.py
@@ -8,3 +8,25 @@ from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
+
+
+app = Flask(__name__)
+
+app.config["DB_NAME"] = os.environ.get("DB_NAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+@app.route("/recipes")
+def recipes():
+    recipes = list(mongo.db.recipes.find())
+    return render_template("recipes.html", recipes=recipes)
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=os.environ.get("PORT"),
+            debug=True)
